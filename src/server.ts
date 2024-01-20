7570b1841a42d27d25f0aea8e82f10cd5b28ae0d
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-01-20 10:53:50 +0600

    [done] Setup winston logger

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,39 @@
-/* eslint-disable no-console */
+import { consoleLogger, errorLogger, logger } from "./shared/logger";
 
-process.on('uncaughtException', error => {
-  console.log(`😓 Uncaught exception happened. Exiting the process.`);
-  console.log('Error:->', error);
+process.on("uncaughtException", error => {
+  consoleLogger.error(
+    "😴 `Uncaught exception` happened, exiting the process and  closing the server.",
+    error,
+  );
   process.exit(1);
 });
 
-import { Server } from 'http';
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config';
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
 
 let server: Server;
 
 const bootstrap = async () => {
   try {
     await mongoose.connect(config.DBUrl as string);
-    console.log(`===${config.env}===`);
-    console.log(`👌 Database is connected successfully.`);
+    consoleLogger.info(`===${config.env}===`);
+    logger.info(`👌 Database is connected successfully.`);
     server = app.listen(config.port, () => {
-      console.log(
+      logger.info(
         `😍 The server is running on http://localhost:${config.port}`,
       );
     });
   } catch (error) {
-    console.log(`❌ Can't connect to Database.`);
-    console.log('Error:->', error);
+    errorLogger.error(`❌ Can't connect to Database.`, error);
   }
 
-  process.on('unhandledRejection', error => {
-    console.log(`😴 Unhandled rejection happened. Exiting the process.`);
-    console.log('Error:->', error);
+  process.on("unhandledRejection", error => {
+    errorLogger.error(
+      `😴 Unhandled rejection happened. Exiting the process.`,
+      error,
+    );
     if (server) {
       server.close(() => {
         process.exit(1);
