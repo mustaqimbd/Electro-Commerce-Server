2d674ff53c508be567c3a1b2f781546c6242b5c7
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-01-20 11:48:44 +0600

    [done] Create global error handler and not fount route handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express, { Application } from "express";
+import globalErrorhandler from "./app/middlewares/globalErrorHandler";
+import { notFoundRoute } from "./app/middlewares/notFoundRoute";
 
 const app: Application = express();
+
+// Global error handler
+app.use(globalErrorhandler);
+
+// handle not found route
+app.use(notFoundRoute);
 
 export default app;
