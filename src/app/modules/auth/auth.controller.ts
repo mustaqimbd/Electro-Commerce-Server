a43ed2923b01aaf4aeab2e2a4df3043d5d254d67
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-01-23 22:57:32 +0600

    [done] create login, refresh token, logout api

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,20 +1,38 @@
-import { Request, Response } from "express";
+import { CookieOptions, Request, Response } from "express";
 import httpStatus from "http-status";
+import config from "../../config/config";
 import catchAsync from "../../utilities/catchAsync";
 import successResponse from "../../utilities/successResponse";
-import { ILoginResponse } from "./auth.interface";
-import { AUthService } from "./auth.service";
+import { TLoginResponse, TRefreshTokenResponse } from "./auth.interface";
+import { AuthServices } from "./auth.service";
 
 const login = catchAsync(async (req: Request, res: Response) => {
   const { ...payload } = req.body;
-  const result = await AUthService.login(payload);
-  successResponse<ILoginResponse>(res, {
-    statusCode: httpStatus.CREATED,
+  const result = await AuthServices.login(payload);
+  const { refreshToken, ...others } = result;
+
+  const cookieOption: CookieOptions = {
+    secure: config.env === "production",
+    httpOnly: true,
+  };
+  res.cookie("refreshToken", refreshToken, cookieOption);
+  successResponse<TLoginResponse>(res, {
+    statusCode: httpStatus.OK,
     message: "Logged in successfully",
+    data: others,
+  });
+});
+
+const refreshToken = catchAsync(async (req: Request, res: Response) => {
+  const { refreshToken } = req.cookies;
+  const result = await AuthServices.refreshToken(refreshToken);
+  successResponse<TRefreshTokenResponse>(res, {
+    statusCode: httpStatus.OK,
     data: result,
   });
 });
 
-export const AuthController = {
+export const AuthControllers = {
   login,
+  refreshToken,
 };
