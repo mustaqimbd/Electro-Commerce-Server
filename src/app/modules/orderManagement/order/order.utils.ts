c36e8b2221c82993b7008891b3c5d51f4d325dec
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-03-12 16:30:13 +0600

    [fix] order service

--- src/app/modules/orderManagement/order/order.utils.ts
+++ src/app/modules/orderManagement/order/order.utils.ts
@@ -1,14 +1,15 @@
 const createOrderId = () => {
-  const getRandomLetter = () => {
-    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
-    return alphabet[Math.floor(Math.random() * alphabet.length)];
-  };
+  // const getRandomLetter = () => {
+  //   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
+  //   return alphabet[Math.floor(Math.random() * alphabet.length)];
+  // };
   const date = new Date();
   const timestamp = date.getTime();
   const randomNum = Math.floor(Math.random() * 9000) + 1000;
-  const randomLetters = Array.from({ length: 5 }, getRandomLetter).join("");
+  // const randomLetters = Array.from({ length: 5 }, getRandomLetter).join("");
+  const randomLetters = "";
   const orderId = `${String(date.getFullYear()).slice(2)}${randomLetters}${date.getMonth() + 1}${date.getDate()}${randomNum}${String(timestamp).split("").reverse().join("")}`;
-  return orderId.slice(0, 15);
+  return orderId.slice(0, 10);
 };
 
 export default createOrderId;
