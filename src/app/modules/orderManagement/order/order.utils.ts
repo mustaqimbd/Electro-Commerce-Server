0939552ef54f4779cfd1c6f2cb36474323329040
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-04-21 20:29:14 +0600

    [done] modified order status change API

--- src/app/modules/orderManagement/order/order.utils.ts
+++ src/app/modules/orderManagement/order/order.utils.ts
@@ -1,3 +1,10 @@
+import httpStatus from "http-status";
+import mongoose from "mongoose";
+import ApiError from "../../../errorHandlers/ApiError";
+import { InventoryModel } from "../../productManagement/inventory/inventory.model";
+import ProductModel from "../../productManagement/product/product.model";
+import { TProductDetails } from "./order.interface";
+
 const createOrderId = () => {
   const date = new Date();
   const timestamp = date.getTime();
@@ -7,4 +14,35 @@ const createOrderId = () => {
   return orderId.slice(0, 10);
 };
 
-export default createOrderId;
+const updateStockOnOrderCancelOrDelete = async (
+  productDetails: TProductDetails[],
+  session: mongoose.mongo.ClientSession,
+  inc: boolean = true
+) => {
+  for (const item of productDetails) {
+    const product = await ProductModel.findById(item.product, {
+      inventory: 1,
+    })
+      .session(session)
+      .lean();
+    if (!product) {
+      throw new ApiError(httpStatus.BAD_REQUEST, "Failed to find the product");
+    }
+
+    let updateQuey = item.quantity;
+
+    if (!inc) {
+      updateQuey = -item.quantity;
+    }
+
+    const updateQUantity = await InventoryModel.updateOne(
+      { _id: product.inventory },
+      { $inc: { stockQuantity: updateQuey } }
+    ).session(session);
+    if (!updateQUantity.modifiedCount) {
+      throw new ApiError(httpStatus.BAD_REQUEST, "Failed to update quantity");
+    }
+  }
+};
+
+export { createOrderId, updateStockOnOrderCancelOrDelete };
