8f381840a5ba934f05b10e062a3d685a2f59cb94
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-09-10 11:56:14 +0600

    [done] crud for coupon completed

---
+++ src/app/modules/coupon/coupon.controller.ts
@@ -0,0 +1,49 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utilities/catchAsync";
+import successResponse from "../../utilities/successResponse";
+import { TJwtPayload } from "../authManagement/auth/auth.interface";
+import { CouponServices } from "./coupon.service";
+
+const createCoupon = catchAsync(async (req, res) => {
+  await CouponServices.createCouponIntoDB(
+    req.body,
+    req.user as unknown as TJwtPayload
+  );
+  successResponse(res, {
+    statusCode: httpStatus.CREATED,
+    message: "Coupon created successfully!",
+  });
+});
+
+const getAllCoupons = catchAsync(async (req, res) => {
+  const result = await CouponServices.getAllCouponsFromBD();
+  successResponse(res, {
+    statusCode: httpStatus.CREATED,
+    message: "All available coupon retrieved successfully!",
+    data: result,
+  });
+});
+
+const getSingleCoupon = catchAsync(async (req, res) => {
+  const result = await CouponServices.getSingleCouponFromBD(req.params.code);
+  successResponse(res, {
+    statusCode: httpStatus.CREATED,
+    message: "Coupon data retrieved successfully!",
+    data: result,
+  });
+});
+
+const updateCouponCode = catchAsync(async (req, res) => {
+  await CouponServices.updateCouponCodeIntoDB(req.params.id, req.body);
+  successResponse(res, {
+    statusCode: httpStatus.CREATED,
+    message: "Coupon updated successfully!",
+  });
+});
+
+export const CouponController = {
+  createCoupon,
+  getAllCoupons,
+  getSingleCoupon,
+  updateCouponCode,
+};
