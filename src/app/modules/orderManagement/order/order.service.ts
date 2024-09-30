94a623ad713975d99d930e964e5700e3bc826a72
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-09-30 14:53:56 +0600

    [fix] some spelling mistakes

--- src/app/modules/orderManagement/order/order.service.ts
+++ src/app/modules/orderManagement/order/order.service.ts
@@ -541,7 +541,7 @@ const updateOrderStatusIntoDB = async (
   }
 ): Promise<void> => {
   // From this api admin can only change this orders
-  const changableOrders: Partial<TOrderStatus[]> = [
+  const changeableOrders: Partial<TOrderStatus[]> = [
     "pending",
     "confirmed",
     "follow up",
@@ -550,7 +550,7 @@ const updateOrderStatusIntoDB = async (
 
   // From this API, admins can only change to this status below.
   const acceptableStatus: Partial<TOrderStatus[]> = [
-    ...changableOrders,
+    ...changeableOrders,
     "processing",
     "canceled",
     "deleted",
@@ -575,11 +575,10 @@ const updateOrderStatusIntoDB = async (
 
     const pipeline: PipelineStage[] = OrderHelper.orderStatusUpdatingPipeline(
       payload.orderIds,
-      changableOrders
+      changeableOrders
     );
 
     const orders = (await Order.aggregate(pipeline)) as Partial<TOrder[]>;
-
     const statusUpdateQuery: {
       updateOne: {
         filter: {
@@ -686,7 +685,7 @@ const updateProcessingStatusIntoDB = async (
   status: Partial<TOrderStatus>,
   user: TJwtPayload
 ) => {
-  const changableStatus: Partial<TOrderStatus[]> = [
+  const changeableStatus: Partial<TOrderStatus[]> = [
     "processing",
     "warranty added",
     "processing done",
@@ -708,7 +707,7 @@ const updateProcessingStatusIntoDB = async (
     session.startTransaction();
     const pipeline = OrderHelper.orderStatusUpdatingPipeline(
       orderIds,
-      changableStatus
+      changeableStatus
     );
     const orders = await Order.aggregate(pipeline).session(session);
 
